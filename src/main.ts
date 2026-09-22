16b61e3feca44708977b35443cf24ed82a9d7e60
Glenn Vockings <glennvockings@gmail.com>
2026-09-22 22:58:30 +0100

    Use trusted origins for API CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
 import type { OraklConfiguration } from './config/configuration';
 
 async function bootstrap() {
@@ -10,21 +12,31 @@ async function bootstrap() {
     bodyParser: false,
   });
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const config =
+    app.get<ConfigService<OraklConfiguration, true>>(ConfigService);
+
+  const trustedOrigins = config.get('auth.trustedOrigins', {
+    infer: true,
+  });
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: trustedOrigins,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  const config =
-    app.get<ConfigService<OraklConfiguration, true>>(ConfigService);
-
   const port = config.get('app.port', {
     infer: true,
   });
+
   await app.listen(port, '0.0.0.0');
 }
 
